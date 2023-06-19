d5a9bf2fed5e4a47208d9dbb4e726473dee03553
Blessing <blessedtgirl@gmail.com>
2023-06-19 07:11:58 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*  main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+int m;
+
+for (n  = 0; n < 100; n++)
+{
+	for (m  = 0; m < 100; m++)
+{
+	if (n < m)
+	{
+		putchar((n / 10) + 48);
+		putchar((n % 10) + 48);
+		putchar(' ');
+		putchar((m / 10)  + 48);
+		putchar((m % 10) + 48);
+		if (n != 98 || m != 99)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+	}
+}
+}
+putchar('\n');
+return (0);
+}
