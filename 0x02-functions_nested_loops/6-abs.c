b11270894b10734a3295e216baad48a4eefa3bd9
Blessing <blessedtgirl@gmail.com>
2023-06-20 08:30:55 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * 6-abs.c - computes the absolur value of an integer
+ * @i: input number as an integer
+ * Return: Absolute value
+ */
+int _abs(int i);
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
