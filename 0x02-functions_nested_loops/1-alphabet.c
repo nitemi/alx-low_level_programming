03517c7cc6731f901f240d6af60b1fb7402a2410
Blessing <blessedtgirl@gmail.com>
2023-06-20 07:25:54 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * 1-alphabet.c-prints the alphabet, in lowercase
- * Return: Always 0
+ * main -Prints the alphabet, in lowercase
+ *
+ * Return: Always 0.
 */
-int void print_alphabet(void)
+void print_alphabet(void)
 {
-	int c;
+	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
 }
