39dc2630a2d0544a3cea331b7d96cf1e80fae804
Blessing <blessedtgirl@gmail.com>
2023-06-20 20:32:00 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,29 +3,25 @@
 /**
  * jack_bauer - prints the minutes of a day
  *
- * Return: no return
  */
 void jack_bauer(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 2; a++)
+int a, b;
+a = 0;
+
+while (a < 24)
 {
-for (b = 0; b <= 9; b++)
+b = 0;
+while (b < 60)
 {
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-	if (a >= 2 && b >= 4)
-		break;
-	_putchar(a + 48);
-	_putchar(b + 48);
-	_putchar(58);
-	_putchar(c + 48);
-	_putchar(d + 48);
+	_putchar((a / 10) + '0');
+	_putchar((a % 10) + '0');
+	_putchar(':');
+	_putchar((b / 10) + '0');
+	_putchar((b % 10) + '0');
 	_putchar('\n');
+	b++
 }
-}
-}
+a++
 }
 }
