7b1605c2a7b3829a3a18ce8accc3ace873cda39a
Blessing <blessedtgirl@gmail.com>
2023-06-23 14:19:40 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	char c;
+int i;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+i = 0;
+positive_or_negative(i);
 
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-
-	return (0);
+return (0);
 }
