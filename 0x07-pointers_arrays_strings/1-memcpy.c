cb03110fe0153edc74ca439ff01f40c118ce2f96
Blessing <blessedtgirl@gmail.com>
2023-07-03 21:09:02 -0700

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,18 +6,28 @@
 *@src: memory where is copied
 *@n: number of bytes
 *
-*Return: copied memory with n byted changed
+*Return: copied memory with n bytes changed
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
-	int r = 0;
-	int i = n;
+
+int r = 0;
+
+int i = n;
+
 
 for (; r < i; r++)
+
 {
-	dest[r] = src[r];
-	n--;
+
+dest[r] = src[r];
+
+n--;
+
 }
-return (dest)
+
+return (dest);
+
 }
